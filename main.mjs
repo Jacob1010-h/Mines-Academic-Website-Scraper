db3d1f0cbf60586b353e7815cd6970e7b97087ea
Jacob1010-h <jacobhotz01@gmail.com>
2025-09-04 15:59:34 -0600

    make some progress

--- main.mjs
+++
@@ -1,8 +0,0 @@
-import getCalendar from './getCalendar.mjs';
-// Scraping options
-const options = {
-  urls: ['https://www.mines.edu/registrar/academic-calendar/'],
-  directory: './result',
-};
-
-getCalendar(options);
---
+++ main.py
@@ -0,0 +1,109 @@
+import re
+from pprint import pprint
+
+import pdfplumber
+import requests
+from datetime import datetime
+import pytz
+# for tree traversal scraping in webpage
+from bs4 import BeautifulSoup
+
+# for input and output operations
+import io
+from icalendar import *
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0'}
+
+
+def get_cal_website():
+    url = "https://www.mines.edu/registrar/academic-calendar/"
+    read = requests.get(url, headers=headers)
+    return read.content
+
+def get_all_pdf_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('a')
+    pdfs = []
+    for link in links:
+        if link.has_attr('href'):
+            link = link['href']
+            if link.endswith('.pdf'):
+                pdfs += [link]
+
+    return pdfs
+
+def get_tables(pdf_links, desired_pdf):
+    response = requests.get(pdfs[int(desired_pdf) - 1], headers=headers)
+    pdf_to_bytes = io.BytesIO(response.content)
+    with pdfplumber.open(pdf_to_bytes) as pdf_links:
+        all_tables = []
+
+        for i, page in enumerate(pdf_links.pages):
+
+            tables = page.extract_tables()
+            # print (tables)
+            if tables:
+                for table in tables:
+                    array_table = [list(row) for row in table]
+                    transposed_table = list(map(list, zip(*array_table)))
+                    all_tables.append(transposed_table)
+            else:
+                print("No Table found on page %d" % (i+1))
+            print("------End of Page %d------" % (i+1))
+    return all_tables
+
+if __name__ == '__main__':
+    html = get_cal_website()
+    pdfs = get_all_pdf_links(html)
+
+    year = input("What calendar year are you looking for? (format: YYYY)")
+
+    # get all the names for each link
+    pdf_names = []
+    for i in range(len(pdfs) - 1, -1, -1):
+        if year not in pdfs[i]:
+            pdfs.pop(i)
+            continue
+        split = pdfs[i].split('/')
+
+        # get the end of the link
+        pdf_names += split[-1:]
+
+    print("Here are the pdfs with the desired year...")
+    if not pdfs:
+        raise Exception("No PDFs found for %s" % year)
+
+    for i, pdf in enumerate(pdfs):
+        print("PDF %d/%d" % (i + 1, len(pdfs)))
+        print("Name: " + pdf_names[i])
+        print("Link: " + pdf)
+        print("--------------")
+
+    desired_pdf = input("What pdf are you looking for?")
+    print("\n\n\n")
+    print("Retrieving pdf information...")
+
+    all_tables = get_tables(pdf_links=pdfs, desired_pdf=desired_pdf)
+
+    # pprint(all_tables)
+
+    ical = Calendar()
+
+    names = []
+    last_index = -1
+    for i,col in enumerate(all_tables):
+        for item in col:
+            if re.match(r"^Fall \d{4}$", item[0].capitalize())
+                    or re.match(r"^Fall \d{4}$", item[0].capitalize()):
+                last_index = i
+            if last_index is i:
+                names += item
+        print(col[0])
+        print("----")
+
+    print(names)
+
+    # event = Event()
+    # event.add('name', name)
+    # event.add('dtstart', datetime(year, month, day, 0,0,0, 0, tzinfo=pytz.utc))
+    # event.add('dtend', datetime(year, month, day,0,0,0,0,tzinfo=pytz.utc))
